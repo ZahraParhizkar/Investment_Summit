7f27d3d4c9babf492d12ed399dbaafa1dca0d2ae
ZahraParhizkar <zahra.parhizkar99@gmail.com>
2025-11-08 11:13:59 +0330

    Update investment_summit.py

--- investment_summit.py
+++ investment_summit.py
@@ -104,19 +104,3 @@ for stage in stages:
         st.markdown(f"<div dir='rtl'><b>📄 خروجی:</b> {stage['output']}</div>", unsafe_allow_html=True)
         st.markdown("<hr>", unsafe_allow_html=True)
 st.image("logo-removebg-preview.png", use_column_width=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
