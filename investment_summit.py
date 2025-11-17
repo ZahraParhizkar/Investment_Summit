775017fe808f75b9e9e449fb4d319539b0730a63
ZahraParhizkar <zahra.parhizkar99@gmail.com>
2025-11-17 09:51:07 +0330

    Update investment_summit.py

--- investment_summit.py
+++ investment_summit.py
@@ -7,20 +7,6 @@ st.image("POWERLOGO.png", use_column_width=True)
 # -----------------------------
 st.set_page_config(page_title="Investment Summit - Health Science and Technology Park", layout="centered")
 
-
-st.markdown("""
-<style>
-@font-face {
-    font-family: 'BNazanin';
-    src: url('https://cdn.jsdelivr.net/gh/rastikerdar/bnazanin-font/BNazanin.ttf') format('truetype');
-}
-
-/* اعمال فونت روی همه عناصر متن */
-p, h1, h2, h3, h4, h5, h6 {
-    font-family: 'BNazanin' !important;
-}
-</style>
-""", unsafe_allow_html=True)
 #*******************************************************************************
 
 #*******************************************************************************
@@ -137,6 +123,7 @@ st.progress(progress)
 
 
 st.image("logo-removebg-preview.png", use_column_width=True)
+
 
 
 
