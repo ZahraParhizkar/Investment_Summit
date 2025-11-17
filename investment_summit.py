3531fd169bb5cb4004ee5c3c01e0e44d251dabbe
ZahraParhizkar <zahra.parhizkar99@gmail.com>
2025-11-17 11:03:14 +0330

    Update investment_summit.py

--- investment_summit.py
+++ investment_summit.py
@@ -84,7 +84,7 @@ stages = [
 
            روز سوم: امکان سنجی از نظر مالی (ارزیابی اقتصادی طبق داده های بدست آمده از هزینه-فروش
 
-           روز چهارم: نگارش Business Plan  """
+           روز چهارم: نگارش Business Plan"""
         
     },
     {
@@ -140,6 +140,7 @@ for stage in stages:
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)  # چند خط فاصله
 st.image("lastpic.png", use_column_width=True)
+
 
 
 
